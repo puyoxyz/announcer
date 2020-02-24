dc55a1b00419acf5a74fa0e8f26e94a88d8477a2
daniel11420 <kiziolek06@gmail.com>
2020-02-24 17:58:23 +0100

    First complete version

--- example_local/config.js
+++ example_local/config.js
@@ -1,10 +1,11 @@
 const config = {
     bot: {
         prefix: 'announcer:', // Put the prefix you want commands to start with here
-        token: 'Put_your_discord_token_here' // Put your Discord Bot Token here
+        token: 'Put_your_discord_token_here', // Put your Discord Bot Token here
+        gameName: 'Game Name' // Put the playing status you want here
     },
 
-    configVersion: '1' //Don't change this!
+    configVersion: '2' //Don't change this!
 }
 
 module.exports = config;
--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const token = config.bot.token;
 const prefix = config.bot.prefix;
 
 const discord = require('discord.js');
-var client = new discord.Client()
+let client = new discord.Client()
 
 client.on('ready', () => {
     console.log('\033[2J'); // Send special character that clears screen
 
     // Set client presence
-    client.user.setPresence({ game: { name: "cpback.net" }, status: 'online'})
+    client.user.setPresence({ game: { name: config.bot.gameName }, status: 'online'}) //set bot's status
         .then()
         .catch(console.error);
     
@@ -17,4 +17,39 @@ client.on('ready', () => {
     console.log("Running on account " + client.user.tag);
 });
 
-client.login(token);
+client.on("message", (message => {
+    if (message.author.bot) return; //Ignore other bots 
+
+    let LowerContent = message.content.toLowerCase(); // Get message content in lowercase and put it in a var
+
+    if (LowerContent.startsWith(prefix + "announce ")) {
+        if (!message.member.roles.find(r => r.name === "unb-mod")) return; // Stop running command if user doesn't have unb-mod
+
+        let PingType = "none"; // define ping type. no ping by default
+
+        commandContent = message.content.slice(prefix.length + 9); // get command content without the actual command
+        if (commandContent.startsWith("--everyone")) {
+            PingType = "everyone"; // if the flag --everyone is there, set ping type to ping everyone
+            contentToAnnounce = commandContent.slice(11); // get the message it should announce
+        } else if (commandContent.startsWith("--here")) {
+            PingType = "here"; // if the flag --here is there, set ping type to ping here
+            contentToAnnounce = commandContent.slice(7); // get the message it should announce
+        } else {
+            contentToAnnounce = commandContent; // if no ping flag, just set the content it should announce to the command content
+        }
+
+        var Embed = new discord.RichEmbed() // make the embed
+        .setDescription(contentToAnnounce)
+        .setTimestamp(Date.now());
+
+        if (PingType == "everyone") {
+            message.channel.send("@everyone", Embed); //send the announcement
+        } else if (PingType == "here") {
+            message.channel.send("@here", Embed); //send the announcement
+        } else {
+            message.channel.send(Embed); //send the announcement
+        }
+    }
+}));
+
+client.login(token); // log in to discord
